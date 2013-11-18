80999188a4d17b7fc9d4e5b8c832e7942da666d5
Michael Bottjer <michaelbottjer@hotmail.com>
2013-11-18 18:54:26 +0000

    initial commit

---
+++ features/post.feature
@@ -0,0 +1,8 @@
+Feature: Submitting Posts 
+  
+  Scenario: Submitting Posts 
+    Given I am on the new post page
+    And I fill in "Title" with "Hello World"
+    And I fill in "Text" with "Hello this is my blog"
+    When I press "Save Post"
+    Then I should see "Hello World"
---
+++ features/step_definitions/post_steps.rb
@@ -0,0 +1,16 @@
+Given(/^I am on the new post page$/) do
+  visit '/posts/new'
+end
+
+Given(/^I fill in "(.*?)" with "(.*?)"$/) do |arg1, arg2|
+  fill_in arg1, :with => arg2
+end
+
+When(/^I press "(.*?)"$/) do |arg1|
+  click_button(arg1)
+end
+
+
+Then(/^I should see "(.*?)"$/) do |arg1|
+  has_content?(arg1)
+end
